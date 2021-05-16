1da2c079ba2666480f369edf0ef281d261443074
kodlovis <418974104@qq.com>
2021-05-16 11:02:39 +0800

    外部用户传参查询

---
+++ server/router/rms/external_users.go
@@ -0,0 +1,13 @@
+package rms
+
+import (
+	"gin-vue-admin/api/v1/rms"
+	"github.com/gin-gonic/gin"
+)
+
+func InitExternalUsersRouter(Router *gin.RouterGroup) {
+	ExternalUsersRouter := Router.Group("ExternalUsers")
+	{
+		ExternalUsersRouter.GET("getUIList", rms.GetUIList)  // 获取Users列表
+	}
+}
