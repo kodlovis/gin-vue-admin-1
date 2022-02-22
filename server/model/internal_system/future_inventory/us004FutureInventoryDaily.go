46ce83151c80d054ca997c29ae84e2fe3ada93b0
kodlovis <418974104@qq.com>
2022-02-22 09:29:22 +0800

    取消软删除

--- server/model/internal_system/future_inventory/us004FutureInventoryDaily.go
+++ server/model/internal_system/future_inventory/us004FutureInventoryDaily.go
@@ -2,13 +2,12 @@
 package future_inventory
 
 import (
-	"gin-vue-admin/global"
 	"time"
 )
 
 // 如果含有time.Time 请自行import time包
 type Us004FutureInventoryDaily struct {
-	global.GVA_MODEL
+	ID          uint      `json:"ID"  gorm:"column:id;comment:ID;"`
 	Time        time.Time `json:"time" form:"time" gorm:"column:time;comment:;type:date;size:0;"`
 	ProductCode string    `json:"productCode" form:"productCode" gorm:"column:product_code;comment:;type:varchar;"`
 	Volume      string    `json:"volume" form:"volume" gorm:"column:volume;comment:;type:double precision;size:53;"`
