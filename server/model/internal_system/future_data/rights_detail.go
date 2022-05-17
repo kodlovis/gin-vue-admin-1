c26c6959bc3167f161e445773b2b616c48aca71d
kodlovis <418974104@qq.com>
2022-05-17 23:24:15 +0800

    增加列

--- server/model/internal_system/future_data/rights_detail.go
+++ server/model/internal_system/future_data/rights_detail.go
@@ -3,16 +3,18 @@ package future_data
 
 // 如果含有time.Time 请自行import time包
 type RightsDetail struct {
-	ID               uint    `json:"id"  gorm:"column:id;comment:ID;"`
-	Time             string  `json:"time" form:"time" gorm:"column:time;comment:;type:varchar;"`
-	ProductName      string  `json:"productName" form:"productName" gorm:"column:product_name;comment:;type:varchar;"`
-	DepartmentName   string  `json:"departmentName" form:"departmentName" gorm:"column:department_name;comment:;type:varchar;"`
-	ThreeCharges     float64 `json:"threeCharges" form:"threeCharges" gorm:"column:three_charges;comment:;type:double precision;size:53;"`
-	SundryExpense    float64 `json:"sundryExpense" form:"sundryExpense" gorm:"column:sundry_expense;comment:;type:double precision;size:53;"`
-	FutureCharges    float64 `json:"futureCharges" form:"futureCharges" gorm:"column:future_charges;comment:;type:double precision;size:53;"`
-	CumulativeRights float64 `json:"cumulativeRights" form:"cumulativeRights" gorm:"column:cumulative_rights;comment:;type:double precision;size:53;"`
-	AdjustExpense    float64 `json:"adjustExpense" form:"adjustExpense" gorm:"column:adjust_expense;comment:;type:double precision;size:53;"`
-	CustomerProfit   float64 `json:"customerProfit" form:"customerProfit" gorm:"column:customer_profit;comment:;type:double precision;size:53;"`
+	ID                  uint    `json:"id"  gorm:"column:id;comment:ID;"`
+	Time                string  `json:"time" form:"time" gorm:"column:time;comment:;type:varchar;"`
+	ProductName         string  `json:"productName" form:"productName" gorm:"column:product_name;comment:;type:varchar;"`
+	DepartmentName      string  `json:"departmentName" form:"departmentName" gorm:"column:department_name;comment:;type:varchar;"`
+	ThreeCharges        float64 `json:"threeCharges" form:"threeCharges" gorm:"column:three_charges;comment:;type:double precision;size:53;"`
+	SundryExpense       float64 `json:"sundryExpense" form:"sundryExpense" gorm:"column:sundry_expense;comment:;type:double precision;size:53;"`
+	FutureCharges       float64 `json:"futureCharges" form:"futureCharges" gorm:"column:future_charges;comment:;type:double precision;size:53;"`
+	CumulativeRights    float64 `json:"cumulativeRights" form:"cumulativeRights" gorm:"column:cumulative_rights;comment:;type:double precision;size:53;"`
+	AdjustExpense       float64 `json:"adjustExpense" form:"adjustExpense" gorm:"column:adjust_expense;comment:;type:double precision;size:53;"`
+	CustomerProfit      float64 `json:"customerProfit" form:"customerProfit" gorm:"column:customer_profit;comment:;type:double precision;size:53;"`
+	ExchangeGainsLosses float64 `json:"exchangeGainsLosses" form:"exchangeGainsLosses" gorm:"column:exchange_gains_losses;comment:;type:double precision;size:53;"`
+	FinancingInterest   float64 `json:"financingInterest" form:"financingInterest" gorm:"column:financing_interest;comment:;type:double precision;size:53;"`
 }
 
 func (RightsDetail) TableName() string {
